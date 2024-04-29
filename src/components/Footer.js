fed267757f25eebac57a54b0d04981fd7231155a
mumbuamutuku <mumbuamutuku@gmail.com>
2024-04-29 18:32:33 +0300

    main

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { logo, pesapal } from '../assets';
 import { ImGithub } from "react-icons/im";
 import { FaLinkedin } from "react-icons/fa6";
-import {FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa"
+import {FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaHome } from "react-icons/fa"
 import { MdLocationOn } from "react-icons/md";
 import { BsPersonFill, BsPaypal } from "react-icons/bs"
 
@@ -29,20 +29,48 @@ const Footer = () => {
                 <div className="text-base flex flex-col gap-2">
                     <p> Nairobi, Kenya </p>
                     <p> Mobile: +254 720954809 </p>
-                    <p> Phone: +254 733929096 </p>
+                    <p> Phone:  +254 733929096 </p>
                     <p> Email: printcopy16@gmail.com </p>     
                 </div>
             </div>
             <div>
                 <h2 className="text-2xl font-semibold text-white mb-4">Profile </h2>
-                <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
-                    <span>
-                        <BsPersonFill />
-                    </span>
-                    My Account
-                </p>
+                <div className="flex flex-col gap-2 text-base">
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span>
+                            <BsPersonFill />
+                        </span>
+                        My Account
+                    </p>
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span>
+                            <BsPaypal />
+                        </span>
+                        Checkout
+                    </p>
+                    <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span>
+                            <FaHome />
+                        </span>
+                        Order Tracking
+                    </p>
+                        <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
+                        <span>
+                            <MdLocationOn />
+                        </span>
+                        Help & Support
+                    </p>
+                </div>
             </div>
+            <div className="flex flex-col justify-center"> 
+            <input className="bg-tranparent border px-4 py-2 text-sm"
+             placeholder="email"
+             type="text" />
+            <button className="text-sm border text-white border-t-0 hover:bg-gray-900
+            active:bg-white active:text-black"> Subscribe </button>
         </div>
+        </div>
+        
     </div>
   );
 }
