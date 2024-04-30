765f7fde0a10a1f9b61359eae6ddf05635d24372
mumbuamutuku <mumbuamutuku@gmail.com>
2024-04-30 16:49:34 +0300

    service

---
+++ src/pages/Services.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <div>
+            <div>Photocopier Maintenance & Service</div>
+        </div>
+        <div>
+            <div>
+                <h1>Photocopier Servicing </h1>
+                <p>We offer a one off service for your machine. If it hasn't been serviced it could be costing you more than it should to print and us the machine.</p>
+                <button>Learn More</button>
+            </div>
+            <div>
+                <h1>Photocopier Maintenance </h1>
+                <p>To ensure your photocopier is kept in the best condition in order to minimise ‘downtime’, we strongly recommend that you have a maintenance service plan in place.</p>
+                <button>Learn More</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
